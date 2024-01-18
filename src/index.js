94ce6388e42e396c945bb31476c322c103e0854e
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-01-18 20:30:38 +0800

    update herE

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@
 const MESSAGE = window.prompt("Hello can you add your Username here !")
 
 console.log(MESSAGE) ;
-// let username ;
+let username ;
 
-// username = MESSAGE ;
+username = MESSAGE ;
 
-document.getElementById("username").innerHTML = "Thank you for visiting my website " + MESSAGE ; 
+document.getElementById("username").value = "Thank you for visiting my website " + MESSAGE ; 
