0bbe50aed732663492708051961ea84fa211d18f
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-01-18 20:42:28 +0800

    update here

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ let message ;
 
 message = window.alert("Thank you! " + MESSAGE) ;
 
-document.getElementById("username").innerHTML = "Thank you for visiting my website " + message ; 
+document.getElementById("username").innerHTML = MESSAGE ; 
